bddc27b17e096b081d21718b3262d3debf04a95b
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-05-09 21:56:55 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return self.title
